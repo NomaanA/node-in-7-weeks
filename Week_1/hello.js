5516117d9544b0d4ebb6a8898a8391dfabc57719
Nomaan Ahgharian <get@nomaan.me>
2017-02-05 15:50:45 -0500

    added export and require

--- Week_1/hello.js
+++ Week_1/hello.js
@@ -1,2 +1,11 @@
-console.log('hello world');
+exports.world = function () {
+    console.log('hello world');
+}
 
+/* We can write this in es6 as the following:
+
+ exports.world = () => {
+    console.log('hello world');
+ }
+
+*/
